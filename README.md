9864fb875e36fafbd4b439c82f99111b31b483c7
Ser Sc <test@test.com>
2021-01-20 08:25:16 +0000

    First commit bot.py

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # weather_bot
 Бот погоды для telegram
+After setup
+apt install python-pip
+pip3 install pyTelegramBotAPI && pip3 install requests && pip3 install bs4
+add line to crontab "*/1 * * * * python3 /home/ser/py_scr/weather_bot/bot.py"
---
+++ bot.py
@@ -0,0 +1,32 @@
+import requests, bs4, telebot
+
+def parce_weather(city):
+        if city == 'мшинская':
+                site = requests.get("https://meteo7.ru/forecast/59828")
+        elif city == 'приморск':
+                site = requests.get("https://meteo7.ru/forecast/76628")
+        parse = bs4.BeautifulSoup(site.text, "html.parser")
+        weather = parse.select('.textForecast')
+        pogoda3 = weather[1].getText()
+        return pogoda3
+
+bot = telebot.TeleBot("505752923:AAFkeLE4JlmWTOWDhLaYQY79YqrVoSG-vM0")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+#@bot.message_handler(func=lambda message: True)
+#def echo_all(message):
+#	bot.reply_to(message, message.text)
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+	if message.text.lower() == 'мшинская':
+        	bot.send_message(message.chat.id, parce_weather('мшинская'))
+	elif message.text.lower() == 'приморск':
+		bot.send_message(message.chat.id, parce_weather('приморск'))
+
+
+
+bot.polling()
